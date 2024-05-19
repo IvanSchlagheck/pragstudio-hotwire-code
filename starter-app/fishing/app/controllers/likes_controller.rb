a8168232c218e8ab8f17cf0e0726ec7571faec91
IvanSchlagheck <ivansch05@gmail.com>
2024-05-19 14:14:06 -0300

    initial commit

---
+++ starter-app/fishing/app/controllers/likes_controller.rb
@@ -0,0 +1,32 @@
+class LikesController < ApplicationController
+  before_action :require_signin
+  before_action :find_fish_catch
+
+  def create
+    return if existing_like
+
+    like = @fish_catch.likes.create!(user: current_user)
+
+    @fish_catch.my_like = like
+
+    render partial: "activity/fish_catch", locals: { fish_catch: @fish_catch }
+  end
+
+  def destroy
+    return unless (like = existing_like)
+
+    like.destroy!
+
+    render partial: "activity/fish_catch", locals: { fish_catch: @fish_catch }
+  end
+
+private
+
+  def find_fish_catch
+    @fish_catch = FishCatch.find(params[:fish_catch_id])
+  end
+
+  def existing_like
+    @fish_catch.likes.where(user: current_user).first
+  end
+end
